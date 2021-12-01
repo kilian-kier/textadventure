28da520f3373e35091bf8725201ad97fa6d2fbf6
MartinG <stgammar2@bx.fallmerayer.it>
2021-12-01 20:21:11 +0100

    Auraumen 2

--- src/com/textadventure/characters/NPC.java
+++ src/com/textadventure/characters/NPC.java
@@ -2,17 +2,37 @@ package com.textadventure.characters;
 
 import com.textadventure.GameElement;
 
-public class NPC extends GameElement {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class NPC extends GameElement implements Serializable {
+    private String room;
+    public ArrayList<String[]> dialog= new ArrayList<String[]>(); // Array[3]: An index 0 die Froge, an index 1 die Antwort und an index 2 a event (string ) oder null
     public NPC(String name, String description) {
         this.name = name;
         this.description = description;
     }
-    String room;
-    public void setRoom(String room){
-        this.room=room;
+
+    public void setRoom(String room) {
+        this.room = room;
     }
     public String getRoom(){
         return room;
     }
 
+    public void removeAllDialog(){
+        dialog.clear();
+    }
+    public void addDialog(String[] dialog){
+        this.dialog.add(dialog);
+    }
+    public ArrayList<String[]> getDialog(){
+        return this.dialog;
+    }
+    public void removeDialogIndex(int index) throws IndexOutOfBoundsException{
+        dialog.remove(index);
+    }
+    public String[] getDialogIndex(int index) throws IndexOutOfBoundsException{
+        return dialog.get(index);
+    }
 }
--- src/com/textadventure/locations/Exit.java
+++ src/com/textadventure/locations/Exit.java
@@ -1,42 +1,64 @@
 package com.textadventure.locations;
 
 import com.textadventure.GameElement;
-import com.textadventure.characters.Player;
-import com.textadventure.interfaces.Interactable;
 
-public class Exit extends GameElement {
-    private Room destination;
+import java.io.Serializable;
+
+public class Exit extends GameElement implements Serializable {
+    private String destination1;
+    private String destination2;
+    private String room;
 
     public Exit(String name, String description) {
         this.name = name;
-        this.description = description;
+        this.description = description; //Beschreibung 1 isch links vor an @ und die Beschreibung 2 ich rechts noch an @
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
     }
 
 
-    public Room getDestination() {
-        return this.destination;
+    public String getDestination2() {
+        return destination2;
+    }
+
+    public void setDestination2(String destination2) {
+        this.destination2 = destination2;
+    }
+
+    public String getDestination1() {
+        return destination1;
+    }
+
+    public void setDestination1(String destination1) {
+        this.destination1 = destination1;
     }
 
     /*@Override
-    public void look() {
-        System.out.println("Da ist ein Ausgang, der nach " + this.destination.getName() + " führt.");
-    }
+        public void look() {
+            System.out.println("Da ist ein Ausgang, der nach " + this.destination.getName() + " führt.");
+        }
 
-    @Override
-    public void use(Player player) {
-        player.changeRoom(this.name);
-    }
+        @Override
+        public void use(Player player) {
+            player.changeRoom(this.name);
+        }
 
-    @Override
-    public void talk() {
-        System.out.println("Du bist ja lustig im Kopf, erwartest du dir eine Antwort von einem Ausgang?");
-    }
+        @Override
+        public void talk() {
+            System.out.println("Du bist ja lustig im Kopf, erwartest du dir eine Antwort von einem Ausgang?");
+        }
 
-    @Override
-    public void investigate() {
-        System.out.println(this.description);
-    }
-*/
+        @Override
+        public void investigate() {
+            System.out.println(this.description);
+        }
+    */
     public java.lang.String getName() {
         return this.name;
     }
